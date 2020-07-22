d495b959606a9d3d52b6d4f4623818171d7dfeb9
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-07-22 08:48:56 -0700

    added tests for 3 other planets

--- __tests__/galactic-calc.test.js
+++ __tests__/galactic-calc.test.js
@@ -11,4 +11,19 @@ describe('AgePlanet', function() {
     const agePerson = new AgePlanet(36)
     expect (agePerson.planetAge(merc)).toEqual('150');
   });
+
+  it('should return age on Venus', function() {
+    const agePerson = new AgePlanet(36)
+    expect (agePerson.planetAge(venus)).toEqual('58');
+  });
+
+  it('should return age on Mars', function() {
+    const agePerson = new AgePlanet(36)
+    expect (agePerson.planetAge(mars)).toEqual('19');
+  });
+
+  it('should return age on Jupiter', function() {
+    const agePerson = new AgePlanet(36)
+    expect (agePerson.planetAge(jupiter)).toEqual('3');
+  });
 });
